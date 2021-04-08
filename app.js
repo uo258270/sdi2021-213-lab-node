473505c6a9b83db54b0b0e265f6c11996abce98c
uo258270 <uo258270@uniovi.es>
2021-04-08 10:14:46 +0200

    SDI-2021-213-8.2-Arquitectura para el acceso a datos

--- app.js
+++ app.js
@@ -8,18 +8,21 @@ let bodyParser =require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
+let gestorBD = require("./modules/gestorBD.js");
+gestorBD.init(app,mongo);
+
+
 app.use(express.static('public'));
 
 
 // Variables
 app.set('port', 8081);
-app.set('db',' mongodb://admin:<password>@tiendamusica-shard-00-00.gmmk4.mongodb.net:27017,tiendamusica-shard-00-01.gmmk4.mongodb.net:27017,tiendamusica-shard-00-02.gmmk4.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-pxj6cm-shard-0&authSource=admin&retryWrites=true&w=majority');
+app.set('db',' mongodb://admin:admin@tiendamusica-shard-00-00.gmmk4.mongodb.net:27017,tiendamusica-shard-00-01.gmmk4.mongodb.net:27017,tiendamusica-shard-00-02.gmmk4.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-pxj6cm-shard-0&authSource=admin&retryWrites=true&w=majority');
 
 //Rutas/controladores por lógica
-require("./routes/rusuarios.js")(app, swig); // (app, param1, param2, etc.)
-require("./routes/rcanciones.js")(app, swig); // (app, param1, param2, etc.)
-require("./routes/rautores.js")(app, swig);
-require("./routes/rcanciones.js")(app, swig, mongo);
+require("./routes/rusuarios.js")(app, swig, gestorBD); // (app, param1, param2, etc.)
+require("./routes/rcanciones.js")(app, swig, gestorBD); // (app, param1, param2, etc.)
+require("./routes/rautores.js")(app, swig, gestorBD);
 
 app.listen(app.get('port'), function(){
     console.log('Servidor activo');
