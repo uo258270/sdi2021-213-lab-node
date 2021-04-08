3287729087701dc8659e434d93716bcc1c960e00
uo258270 <uo258270@uniovi.es>
2021-04-08 11:00:17 +0200

    “SDI-2021-213-8.5-Colecciones relacionadas y routers.”

--- routes/rcanciones.js
+++ routes/rcanciones.js
@@ -124,4 +124,60 @@ module.exports = function(app, swig, gestorBD) {
         });
     });
 
+
+    app.post('/cancion/modificar/:id', function (req, res) {
+        let id = req.params.id;
+        let criterio = { "_id" : gestorBD.mongo.ObjectID(id) };
+        let cancion = {
+            nombre : req.body.nombre,
+            genero : req.body.genero,
+            precio : req.body.precio
+        }
+        gestorBD.modificarCancion(criterio, cancion, function(result) {
+            if (result == null) {
+                res.send("Error al modificar ");
+            } else {
+                res.send("Modificado "+result);
+                paso1ModificarPortada(req.files, id, function (result) {
+                    if( result == null){
+                        res.send("Error en la modificación");
+                    } else {
+                        res.send("Modificado");
+                    }
+                });
+            }
+        });
+    });
+
+    function paso1ModificarPortada(files, id, callback){
+        if (files && files.portada != null) {
+            let imagen =files.portada;
+            imagen.mv('public/portadas/' + id + '.png', function(err) {
+                if (err) {
+                    callback(null); // ERROR
+                } else {
+                    paso2ModificarAudio(files, id, callback); // SIGUIENTE
+                }
+            });
+        } else {
+            paso2ModificarAudio(files, id, callback); // SIGUIENTE
+        }
+    };
+    function paso2ModificarAudio(files, id, callback){
+        if (files && files.audio != null) {
+            let audio = files.audio;
+            audio.mv('public/audios/'+id+'.mp3', function(err) {
+                if (err) {
+                    callback(null); // ERROR
+                } else {
+                    callback(true); // FIN
+                }
+            });
+        } else {
+            callback(true); // FIN
+        }
+    };
+
+
+
 };
