15228cb61544c54d7259327def683343e51fa759
uo258270 <uo258270@uniovi.es>
2021-04-15 12:17:43 +0200

    SDI-2021-213-9.7-Retoques

--- app.js
+++ app.js
@@ -126,3 +126,11 @@ app.use( function (err, req, res, next) {
     }
 });
 
+
+https.createServer({
+    key: fs.readFileSync('certificates/alice.key'),
+    cert: fs.readFileSync('certificates/alice.crt')
+}, app).listen(app.get('port'), function() {
+    console.log("Servidor activo");
+});
+
